ec095e7393d99a0f4e40d3ac080c0496dc8c484d
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-04-21 19:58:26 +0900

    0421

---
+++ src/chapter05/WeekExample.java
@@ -0,0 +1,38 @@
+package chapter05;
+
+import java.util.Calendar;
+
+public class WeekExample {
+	public static void main(String[] args) {
+		Week today = null;
+		
+		Calendar calender = Calendar.getInstance();
+		int week = calender.get(Calendar.DAY_OF_WEEK); // 일(1)~토(7)
+		System.out.println(week);
+		
+		switch (week) {
+		case 1:
+			today = Week.일;
+			break;
+		case 2:
+			today = Week.월;
+			break;
+		case 3:
+			today = Week.화;
+			break;
+		case 4:
+			today = Week.수;
+			break;
+		case 5:
+			today = Week.목;
+			break;
+		case 6:
+			today = Week.금;
+			break;
+		case 7:
+			today = Week.토;
+			break;
+		}
+		System.out.println("오늘 요일은: "+today+"요일");
+	}
+}
