6219401293b5d1eb5d75aa00ccf4ecc00133419c
Ms-97 <58967787+Ms-97@users.noreply.github.com>
2022-05-04 20:24:21 +0900

    0504(classroomExample)

---
+++ src/chapter07/classroomExample/Customer.java
@@ -0,0 +1,39 @@
+package chapter07.classroomExample;
+
+
+public class Customer {
+	
+	private String firstName;
+	private String lastName;
+	private BankAccount account;
+	
+	
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public BankAccount getAccount() {
+		return account;
+	}
+
+
+	public void setAccount(BankAccount account) {
+		this.account = account;
+	}
+
+
+
+	public String toString() {
+		return String.format("이름: "+firstName+" "+lastName+", 잔고: "+account.getBalance()+"원");
+	}
+}
